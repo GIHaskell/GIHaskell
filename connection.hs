2195847a5eaecbe2f953d9c16ef615a6a7ab3aa1
AlexEllio <AlexEllio@users.noreply.github.com>
2018-12-04 11:46:43 +0100

    Update connection.hs

--- connection.hs
+++ connection.hs
@@ -4,11 +4,21 @@ module Main where
 
 import Database.MySQL.Base
 import qualified System.IO.Streams as Streams
+import qualified Data.Text as T
+import Data.Maybe
 
-main :: IO () 
 main = do
     conn <- connect
         ConnectInfo {ciHost = "jfaldanam.ddns.net", ciPort = 3306, ciDatabase = "GIHaskell",
                      ciUser = "martin", ciPassword = "password_martin", ciCharset = 33}
     (defs, is) <- query_ conn "SELECT * FROM tUsuario"
-    print =<< Streams.toList is
+    
+    xs <- Streams.toList is
+    let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+    print rs      
+        
+        
+-- funciones auxiliarles        
+getString :: MySQLValue -> String
+getString (MySQLText text) = T.unpack text
+        
