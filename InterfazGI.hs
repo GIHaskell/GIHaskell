594db50b69339a757ae288ac862fdccff27dbcba
Darío <37145332+shah-W@users.noreply.github.com>
2018-12-05 12:30:35 +0100

    Add files via upload

--- InterfazGI.hs
+++ InterfazGI.hs
@@ -1,54 +1,100 @@
+printOperats= "i:insertar\tb:borrar\ta:actualizar\t\ts:salir\nS1:Select1\tS2:Select2\tn:cambiar nombre\tf:cambiar fabricante"
+listaClases=["    nº","id","nombre","fabricante","id_tipo"]
+
 main :: IO ()
 main=do
-    putStrLn "Usuario\n"
+    putStrLn "Usuario"
     usr<-getLine
-    putStrLn (usr++"\n")
+    --putStrLn (usr++"\n")
     
-    putStrLn "password\n"
+    putStrLn "password"
     pass<-getLine
-    putStrLn (pass++"\n")
+    --putStrLn (pass++"\n")
     
     --No hacer prints y comparar con la base de datos
     
-    putStrLn "Seleccione el tipo de pieza deseado\n"
-    putStrLn(printTypes ["martillo", "tornillo", "tu prima"] 1)
-    --if type==1 then print"
-    do pruebaIO
     
+    
+    --if type==1 then print"
+    limpiar
+    do actualizarVista 3 1
+    
+    
+    
+    
+--while:: (a->Bool) -> (a->IO a) -> a->IO a
+--while cond funcion x
+--    | cond x = do
+--       y <-funcion x
+--        while cond funcion y
+--    | otherwise = return x
     --principal:: IO ()
-printTypes:: [String]->Int->String
-printTypes (x:xs) n
-    |(not (null xs))&&(n/=1)="()  "++x++"\n"++printTypes xs (n-1)
-    |(not (null xs))&&(n==1)="(*) "++x++"\n"++printTypes xs (n-1)
+printPiezas:: [String]->Int->String
+printPiezas (x:xs) n
+    |(not (null xs))&&(n/=1)="( ) "++x++"\n"++printPiezas xs (n-1)
+    |(not (null xs))&&(n==1)="(*) "++x++"\n"++printPiezas xs (n-1)
     |(null xs)&&(n==1)="(*) "++x++"\n\n"
-    |otherwise="()  "++x++"\n\n"
+    |otherwise="( ) "++x++"\n\n"
     --where contador=1;
     
-printClasses::String
-printClasses="\tnº\tid\tnombre\tfabricante\tid_tipo"
 
-listaClases=["nº","id","nombre","fabricante","id_tipo"]
 
-printClasses2::[String]->Int->String
-printClasses2 (x:xs) n
-    |(not (null xs))&&(n/=1)="()"++x++"\t"++printClasses2 xs (n-1)
-    |(not (null xs))&&(n==1)="(*)"++x++"\t"++printClasses2 xs (n-1)
-    |(null xs)&&(n==1)="(*)"++x
-    |otherwise="()"++x
+
+printCabecera::[String]->String--print calses normal
+printCabecera (x:xs) 
+    |(not (null xs))=x++"\t"++printCabecera xs 
+    |otherwise=x
     
-printSpecs::[String]->String
-printSpecs (x:xs)
-    |(not (null xs))=x++"\t"++printSpecs xs
-    |(null xs)=x++"\n\n"
+datosTabla::[String]->String--printa las specs(datos)
+datosTabla (x:xs) 
+    |(not (null xs))=x++"\t"++datosTabla xs 
+    |otherwise=x++"\n\n"
     --se puede aplicar map para printar lineas de todas las cosas seleccionadas, y un filter para el proceso de seleccion
     
-pruebaIO :: IO ()
-pruebaIO=do
-    putStrLn(printClasses2 listaClases 2)
-    putStrLn(printSpecs ["12","123","martillo","Avotillo","4"])
+marcadorFila::[String]->Int->[String]--Le pasas todos los datos y marca el seleccionado
+marcadorFila (x:xs) n
+    |(not (null xs))&&(n==1)=["(*) "++x]++(marcadorFila xs (n-1))
+    |(not (null xs))&&(n/=1)=["( ) "++x]++(marcadorFila xs (n-1))
+    |(null xs)&&(n==1)=["(*) "++x]
+    |(null xs)&&(n/=1)=["( ) "++x]
+    
+actualizarVista :: Int->Int->IO ()--hay que ponerle 2 int
+actualizarVista x y=do
+    
+    putStrLn "Piezas"
+    putStrLn(printPiezas ["martillo", "tornillo", "tu prima"] x)
+    
+    putStrLn "Piezas del tipo seleccionado"
+    
+    putStrLn(printCabecera listaClases)
+    putStrLn (head (marcadorFila [(datosTabla ["12","123","maza","Avotillo","4"])] y))  
+    putStrLn "\nNombre:"--alasd
+    putStrLn "Fabricante:\n"--asdasdasd
     putStrLn(printOperats)
     
-printOperats:: String
-printOperats= "i: insertar\tb: borrar\ta: actualizar\ts:salir\nS1: Select1\tS2:Select2"
-
+    putStrLn "\nSeleccione una operacion: "
+    operacion<-getLine
+    do logica 2 1 operacion
     
+    
+logica::Int->Int->String->IO()
+logica x y val
+    |val=="i" = do
+        limpiar
+        putStrLn "Insertando...\n\n"
+        actualizarVista x 0
+    |val=="b" = do
+        limpiar
+        putStrLn "Borrando seleccionado...\n\n"
+        actualizarVista x 0
+    |val=="a" = do
+        limpiar
+        putStrLn ("Actualizando valores de fila "++(show y)++"\n\n")
+        actualizarVista x 0
+    |val=="s" =do
+        limpiar
+        putStrLn "| |\n| |__  _   _  ___\n| '_ \\| | | |/ _ \\\n| |_) | |_| |  __/\n|_.__/ \\__, |\\___|\n        __/ |     \n       |___/     :D\n\n\n"
+
+
+limpiar:: IO()
+limpiar= do putStrLn "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
