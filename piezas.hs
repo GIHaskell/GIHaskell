4b407348252265f4cd6b98a96c5457cf9d39eea6
Cristian Rafael Ávila García <36643196+avilavt@users.noreply.github.com>
2018-12-06 16:38:57 +0100

    Mapeo de tPiezas y tTipoPieza

---
+++ piezas.hs
@@ -0,0 +1,158 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Piezas where
+
+import Database.MySQL.Base
+import Database.MySQL.BinLog
+import qualified System.IO.Streams as Streams
+import qualified Data.Text as T
+import Data.Maybe
+import Data.Int (Int8, Int16, Int32, Int64)
+import Data.Char
+import qualified Insert
+
+servidorBD = "jfaldanam.ddns.net"
+usuarioBD = "usuario"
+passwordBD = "password_"
+databaseDB = "GIHaskell"
+puertoBD = 3306
+
+main = do
+ print "Modulo de mapeo de Piezas"
+
+type IdPiezaOriginal = Int32
+type IdPieza = Int32
+type NombrePieza = String
+type FabricantePieza = String
+type IdTipoPieza = String
+
+piezas :: IdPieza -> NombrePieza -> FabricantePieza -> IdTipoPieza -> IO()
+piezas id nombre fabricante idTipo = do
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                   ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+
+  withTransaction conn $ executeMany conn "INSERT INTO tPiezas VALUES (\
+          \?  ,\
+          \?  ,\
+          \?  ,\
+          \?)"
+          [[MySQLInt32 id,
+           MySQLText $ T.pack nombre,
+           MySQLText $ T.pack fabricante,
+           MySQLText $ T.pack idTipo]]
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tPiezas"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+
+
+listaPiezas :: IO [[String]]
+listaPiezas = do
+    conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                    ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+  
+    (defs, is) <- query_ conn "SELECT * FROM tPiezas"
+    xs <- Streams.toList is
+    let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+    --print xs
+    --putStrLn rs
+    return rs
+
+setIdPieza :: IdPiezaOriginal -> IdPieza -> IO()
+setIdPieza pk id = do
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                   ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+
+  updStmt <- prepareStmt conn "UPDATE tPiezas SET ID = ? WHERE ID = ? "
+  
+  executeStmt conn updStmt [MySQLInt32 id,MySQLInt32 pk]
+
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tPiezas"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+
+
+setNombre :: IdPiezaOriginal -> NombrePieza -> IO()
+setNombre pk nombre = do
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                   ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+
+  updStmt <- prepareStmt conn "UPDATE tPiezas SET NOMBRE = ? WHERE ID = ? "
+  
+  executeStmt conn updStmt [MySQLText (T.pack nombre),MySQLInt32 pk]
+
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tPiezas"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+
+setFabricante :: IdPiezaOriginal -> FabricantePieza -> IO()
+setFabricante pk fabricante = do
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                     ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+  
+  updStmt <- prepareStmt conn "UPDATE tPiezas SET FABRICANTE = ? WHERE ID = ? "
+    
+  executeStmt conn updStmt [MySQLText (T.pack fabricante),MySQLInt32 pk]
+  
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tPiezas"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+
+setTipoPieza :: IdPiezaOriginal -> IdTipoPieza -> IO()
+setTipoPieza pk idTipo = do
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                      ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+    
+  updStmt <- prepareStmt conn "UPDATE tPiezas SET ID_TIPO = ? WHERE ID = ? "
+      
+  executeStmt conn updStmt [MySQLText (T.pack idTipo),MySQLInt32 pk]
+    
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tPiezas"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+
+delete :: IdPiezaOriginal -> IO()
+delete pk = do 
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                      ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+    
+  delStmt <- prepareStmt conn "DELETE FROM tPiezas WHERE ID = ? "
+      
+  executeStmt conn delStmt [MySQLInt32 pk]
+    
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tPiezas"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+    
+      
+
+getString :: MySQLValue -> String
+getString (MySQLText text) = T.unpack text
+getString (MySQLInt8 value) = show (fromInt8ToInt value)
+getString (MySQLInt32 value) = show (fromInt32ToInt value)
+getString (MySQLNull) = ""
+  
+fromInt8ToInt :: Int8 -> Int
+fromInt8ToInt n = fromIntegral n
+  
+fromInt32ToInt :: Int32 -> Int
+fromInt32ToInt n = fromIntegral n
+  
---
+++ tipoPieza.hs
@@ -0,0 +1,123 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module TipoPiezas where
+
+import Database.MySQL.Base
+import Database.MySQL.BinLog
+import qualified System.IO.Streams as Streams
+import qualified Data.Text as T
+import Data.Maybe
+import Data.Int (Int8, Int16, Int32, Int64)
+import Data.Char
+import qualified Insert
+
+servidorBD = "jfaldanam.ddns.net"
+usuarioBD = "usuario"
+passwordBD = "password"
+databaseDB = "GIHaskell"
+puertoBD = 3306
+
+main = do
+ print "Modulo de mapeo de Tipo de Piezas"
+
+type IdTipoPiezaOriginAL = String
+type IdTipoPieza = String
+type NombreTipoPieza = String
+
+
+tipoPiezas :: IdTipoPieza -> NombreTipoPieza -> IO()
+tipoPiezas idTipoPieza nombre = do
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                   ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+
+  withTransaction conn $ executeMany conn "INSERT INTO tTipoPieza VALUES (\
+          \?  ,\
+          \?  ,\
+          \?)"
+          [[MySQLText $ T.pack idTipoPieza,
+            MySQLText $ T.pack nombre]]
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tTipoPieza"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+
+
+listaTipoPiezas :: IO [[String]]
+listaTipoPiezas = do
+    conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                    ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+  
+    (defs, is) <- query_ conn "SELECT * FROM tTipoPieza"
+    xs <- Streams.toList is
+    let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+    --print xs
+    --putStrLn rs
+    return rs
+
+setIdTipoPieza :: IdTipoPiezaOriginAL -> IdTipoPieza -> IO()
+setIdTipoPieza pk idTipoPieza = do
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                     ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+  
+  updStmt <- prepareStmt conn "UPDATE tTipoPieza SET ID_TIPO = ? WHERE ID_TIPO = ? "
+    
+  executeStmt conn updStmt [MySQLText (T.pack idTipoPieza),MySQLText (T.pack pk)]
+  
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tTipoPieza"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+
+
+setNombre :: IdTipoPiezaOriginAL -> NombreTipoPieza -> IO()
+setNombre pk nombre = do
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                   ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+
+  updStmt <- prepareStmt conn "UPDATE tTipoPieza SET NOMBRE = ? WHERE ID_TIPO = ? "
+  
+  executeStmt conn updStmt [MySQLText (T.pack nombre),MySQLText (T.pack pk)]
+
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tTipoPieza"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+
+
+delete :: IdTipoPiezaOriginAL -> IO()
+delete pk = do 
+  conn <- connect
+      ConnectInfo {ciHost = servidorBD, ciPort = puertoBD, ciDatabase = databaseDB,
+                      ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
+    
+  delStmt <- prepareStmt conn "DELETE FROM tTipoPieza WHERE ID_TIPO = ? "
+      
+  executeStmt conn delStmt [MySQLText (T.pack pk)]
+    
+  print "Transaccion realizada"
+  --(defs, is) <- query_ conn "SELECT * FROM tTipoPieza"
+  --xs <- Streams.toList is
+  --let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
+  --print rs
+    
+      
+
+getString :: MySQLValue -> String
+getString (MySQLText text) = T.unpack text
+getString (MySQLInt8 value) = show (fromInt8ToInt value)
+getString (MySQLInt32 value) = show (fromInt32ToInt value)
+getString (MySQLNull) = ""
+  
+fromInt8ToInt :: Int8 -> Int
+fromInt8ToInt n = fromIntegral n
+  
+fromInt32ToInt :: Int32 -> Int
+fromInt32ToInt n = fromIntegral n
+  
