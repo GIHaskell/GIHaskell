d2646b6a51bf3c7a0a6f7fa0184c8fe6b5dc69f1
jfaldanam <jfaldanam@gmail.com>
2018-12-06 10:25:49 +0100

    Anadida la posibilidad de modificar el nombre y fabricante

--- InterfazGI.hs
+++ InterfazGI.hs
@@ -1,6 +1,10 @@
 printOperats= "i:insertar\tb:borrar\ta:actualizar\t\te:salir\ns1:Select Tipo\ts2:Select Pieza\tn:cambiar nombre\tf:cambiar fabricante"
 listaClases=["    nº","id","nombre","fabricante","id_tipo"]
-datosPrueba=[["1","123","maza","Avotillo","4"], ["2","77","Martillo","Martilleante","4"],["3","64","Probando","no u","4"],["4","234","si","no","4"]]
+datosPrueba1=[["1","123","aaaa","Avotillo","4"], ["2","77","aaaaaa","Martilleante","4"],["3","64","aaaaaa","no u","4"],["4","234","aaaaaa","no","4"]]
+datosPrueba2=[["2","77","bbbbbb","Martilleante","4"],["1","123","bbbbbb","Avotillo","4"],["3","64","bbbbbbb","no u","4"],["4","234","bbbbbbb","no","4"]]
+datosPrueba3=[["3","64","cccccc","no u","4"],["4","234","cccccccc","no","4"], ["1","123","ccccccc","Avotillo","4"], ["2","77","cccccc","Martilleante","4"]]
+tiposPiezas=["martillo", "tornillo", "tu prima"]
+
 
 main :: IO ()
 main=do
@@ -18,7 +22,7 @@ main=do
 
     --if type==1 then print"
     limpiar
-    do actualizarVista 3 1
+    do actualizarVista [1, 0] ["nombre", "fabricante"]
 
 
 
@@ -59,19 +63,27 @@ marcadorFila (x:xs) n
     |(null xs)&&(n==1)=["(*) "++x]
     |(null xs)&&(n/=1)=["( ) "++x]
 
-actualizarVista :: Int->Int->IO ()--hay que ponerle 2 int
+actualizarVista :: [Int]->[String]->IO ()--hay que ponerle 2 int
 actualizarVista x y=do
 
     putStrLn "Piezas"
-    putStrLn(printPiezas ["martillo", "tornillo", "tu prima"] x)
+    putStrLn(printPiezas tiposPiezas (head x))
 
     putStrLn "Piezas del tipo seleccionado"
 
     putStrLn(printCabecera listaClases)
     --putStrLn (head (marcadorFila [(datosTabla ["12","123","maza","Avotillo","4"])] y))
-    printLista (marcadorFila (map datosTabla datosPrueba) y)
-    putStrLn "\nNombre:"--alasd
-    putStrLn "Fabricante:\n"--asdasdasd
+    --printLista (marcadorFila (map datosTabla datosPrueba1) y)
+    if (head x)==1 then
+      printLista (marcadorFila (map datosTabla datosPrueba1) (head (tail x)))
+    else if (head x)==2 then
+      printLista (marcadorFila (map datosTabla datosPrueba2) (head (tail x)))
+    else if (head x)==3 then
+      printLista (marcadorFila (map datosTabla datosPrueba3) (head (tail x)))
+    else
+      printLista (marcadorFila (map datosTabla datosPrueba1) (head (tail x)))
+    putStrLn ("\nNombre:"++(head y))
+    putStrLn ("Fabricante:"++(head (tail y))++"\n")
     putStrLn(printOperats)
 
     putStrLn "\nSeleccione una operacion: "
@@ -79,20 +91,20 @@ actualizarVista x y=do
     do logica x y operacion
 
 
-logica::Int->Int->String->IO()
+logica::[Int]->[String]->String->IO()
 logica x y val
     |val=="i" = do
         limpiar
         putStrLn "Insertando...\n\n"
-        actualizarVista x 0
+        actualizarVista [(head x), 0] y
     |val=="b" = do
         limpiar
         putStrLn "Borrando seleccionado...\n\n"
-        actualizarVista x 0
+        actualizarVista [(head x), 0] y
     |val=="a" = do
         limpiar
         putStrLn ("Actualizando valores de fila "++(show y)++"\n\n")
-        actualizarVista x 0
+        actualizarVista x y
     |val=="e" = do
         limpiar
         putStrLn "| |\n| |__  _   _  ___\n| '_ \\| | | |/ _ \\\n| |_) | |_| |  __/\n|_.__/ \\__, |\\___|\n        __/ |     \n       |___/     :D\n\n\n"
@@ -101,13 +113,25 @@ logica x y val
         seleccion<-getLine
         limpiar
         putStrLn ("Seleccionada tipo de pieza "++seleccion++"\n\n")
-        actualizarVista (read seleccion) 0
+        actualizarVista [(read seleccion), 0] y
     |val=="s2" = do
         putStrLn "\nInserte indice de pieza a seleccionar:"
         seleccion<-getLine
         limpiar
         putStrLn ("Seleccionada pieza "++seleccion++"\n\n")
-        actualizarVista x (read seleccion)
+        actualizarVista [(head x),(read seleccion)] y
+    |val=="n" = do
+        putStrLn "\nInserte el nuevo nombre:"
+        nombre<-getLine
+        limpiar
+        putStrLn ("Actualizando nombre a: "++nombre++"\n\n")
+        actualizarVista x [nombre,(head (tail y))]
+    |val=="f" = do
+        putStrLn "\nInserte el nuevo fabricante:"
+        fabricante<-getLine
+        limpiar
+        putStrLn ("Actualizando fabricante a: "++fabricante++"\n\n")
+        actualizarVista x [(head y),fabricante]
     |otherwise = do
         putStrLn "\nComando desconocido, intentelo de nuevo:"
         operacion<-getLine
