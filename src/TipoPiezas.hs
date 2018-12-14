dc32b2837c964b4f253fb2059fbc62ce414ae394
Cristian Rafael Ávila García <36643196+avilavt@users.noreply.github.com>
2018-12-14 22:28:43 +0100

    Update TipoPiezas.hs

--- src/TipoPiezas.hs
+++ src/TipoPiezas.hs
@@ -48,10 +48,10 @@ listaTipoPiezas = do
                     ciUser = usuarioBD, ciPassword = passwordBD, ciCharset = 33}
 
     (defs, is) <- query_ conn "SELECT * FROM tTipoPieza"
-    aux <- close conn
+    
     xs <- Streams.toList is
     let rs = [ [getString x | x <- y ] | y <- xs] -- unpack convierte Text a String
-
+    aux <- close conn
     return rs
 
 setIdTipoPieza :: IdTipoPiezaOriginAL -> IdTipoPieza -> IO()
